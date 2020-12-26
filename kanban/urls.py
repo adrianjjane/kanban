a8d149e47692aca7df6afe4f6608dcf3c855ebf5
Adrian Jane <adrianjj@umich.edu>
2020-12-26 13:46:31 -0500

    Fix bug when serving index.html
    Only served index.html on the main route. Fixed by replacing with
    a catchall route.

--- kanban/urls.py
+++ kanban/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 import debug_toolbar
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
@@ -24,5 +24,5 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*/$', TemplateView.as_view(template_name='index.html')),
 ]
