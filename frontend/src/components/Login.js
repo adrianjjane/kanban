82af7f12e1666a32042bf344d51d99597dc03fc9
Adrian Jane <adrianjj@umich.edu>
2020-12-26 22:18:48 -0500

    Add login form to login page
    Login form updates on user input but does not send a POST request
    on submit.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,3 +1,56 @@
-export default function Login() {
-  return <h2>Login</h2>;
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username:
+            <input 
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button type="submit">
+            Log In
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default Login;
