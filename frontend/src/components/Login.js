9fc827f34e446382ba8050da15e44b5cfb73d779
Adrian Jane <adrianjj@umich.edu>
2021-01-12 16:44:45 -0500

    Send POST request to auth api with login credentials

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -5,7 +5,7 @@ class Login extends React.Component {
     super(props);
     this.state = {
       username: '',
-      password: ''
+      password: '',
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -13,12 +13,39 @@ class Login extends React.Component {
 
   handleChange(e) {
     this.setState({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+
+    // attempt to log in user
+    fetch('/auth/token/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      }),
+    })
+    .then((response) => {
+      if (!response.ok) throw new Error(response.statusText);
+      return response.json();
+    })
+    .then((data) => {
+      // reset form
+      this.setState({
+        username: '',
+        password: '',
+      });
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   }
 
   render() {
@@ -28,7 +55,7 @@ class Login extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label>
             Username:
-            <input 
+            <input
               type="text"
               name="username"
               value={this.state.username}
